8b0adefc4147ea3a0567312092de963145bd0b3e
Tadas Sasnauskas <tadas@refme.com>
2015-04-09 10:30:54 +0100

    Issue: replay seems to be incompatible with npm package 'request' + timeout option

--- example/test.js
+++ example/test.js
@@ -1,19 +1,32 @@
-assert = require("assert")
-http = require("http")
-replay = require("../lib/replay")
+assert = require("assert");
+http = require("http");
+request = require("request");
+replay = require("../lib/replay");
 
-http.get({ hostname: "www.iheartquotes.com", path: "/api/v1/random" }, function(response) {
+http.get("htt://www.iheartquotes.com/api/v1/random", function(response) {
   response.body = "";
+  response.on("error", function(err) {
+    console.log(err);
+  });
   response.on("data", function(chunk) {
     response.body = response.body + chunk;
-  })
+  });
   response.on("end", function() {
-
     // Now check the request we made to the I <3 Quotes API
     assert.equal(response.statusCode, 200);
-    assert.equal(response.body, "Oxymoron 2. Exact estimate\n\n[codehappy] http://iheartquotes.com/fortune/show/38021\n");
-    console.log("Woot!");
-
+    assert.equal(response.body, "OK, so you're a Ph.D.  Just don't touch anything.\n\n[fortune] http://iheartquotes.com/fortune/show/46398\n");
+    console.log("http.get works. Woot!");
   })
-})
+});
 
+request.get("http://www.iheartquotes.com/api/v1/random", {}, function(error, response, body) {
+  assert.equal(response.statusCode, 200);
+  assert.equal(body, "OK, so you're a Ph.D.  Just don't touch anything.\n\n[fortune] http://iheartquotes.com/fortune/show/46398\n");
+  console.log("request.get *without* timeout works. Woot!");
+});
+
+request.get("http://www.iheartquotes.com/api/v1/random", {timeout: 30000}, function(error, response, body) {
+  assert.equal(response.statusCode, 200);
+  assert.equal(body, "OK, so you're a Ph.D.  Just don't touch anything.\n\n[fortune] http://iheartquotes.com/fortune/show/46398\n");
+  console.log("request.get with timeout works. Woot!");
+});
